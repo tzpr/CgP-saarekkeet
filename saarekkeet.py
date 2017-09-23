1a02fe8c555ff71d7952b50d5b10bcdcf34b3673
Toni Räsänen <toni.rasanen@iki.fi>
2017-09-23 21:08:51 +0300

    basic input handling. fasta from internet.

--- saarekkeet.py
+++ saarekkeet.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import Bio.Entrez
+import Bio.SeqIO
 
 
-def read_fasta_file_from_the_internet(organism_name):
+def read_fasta_file_from_the_internet(organism_name, email):
     '''
     Try to find genome for given organism from the internet in a fasta format.
+    (http://biopython.org/DIST/docs/api/Bio.Entrez-module.html)
+    
     Throws exception in case of an unknown organism.
     '''
+    # TODO verify email and organism_name
+    
+    # working implementation
+    # Bio.Entrez.email = email
+    #handle = Bio.Entrez.efetch(db="nucleotide", rettype="gb", retmode="text", id=organism_name)
+    #seq_record = Bio.SeqIO.read(handle, "gb")
+    #handle.close()
+    #print("Genbank ID:", seq_record.id)
+    #print("Annotations:", seq_record.annotations)
+    #print("Features:", seq_record.features)
+    #print("Sekvenssi:", seq_record.seq)    
+    
+    
     print('read_fast_from_the_internet')
     
 
@@ -63,9 +80,25 @@ def start():
     '''
     The main thing.
     '''
+    test_fasta_file = 'SH1_genome.fasta'
+    
+    organism1 = input('Anna eliö 1: ')
+    organism2 = input('Anna eliö 2: ')
+    email = input('Anna email: ')
+    
+    if(organism1 == 'testing'):
+        fasta_for_org1  = read_fasta_file_from_filesystem(test_fasta_file)
+    elif(organism1 is not '' and email is not ''):
+        fasta_for_org1 = read_fasta_file_from_the_internet(organism1, email)
+        if(organism2 is not ''):
+            fasta_for_org2 = read_fasta_file_from_the_internet(organism2, email)
+    else:
+        if(organism1 is ''):
+            print('Anna jokin eliö!')
+        if(email is ''):
+            print('Sähköpostiosoite tarvitaan internet-hakuun!')
     
     print('started!')
-
 
 
 # guard to only execute code when a file is invoked as a script
